b21c303326293d0d28c65a2528b2706c322cd0d0
Dev <macbook@MacBook-Pro--AbuBakr.local>
2026-04-13 09:48:01 +0500

    build: remove standalone output to fix pm2 next start

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  allowedDevOrigins: ["helena-compressed-ungladly.ngrok-free.dev"],
   images: {
     remotePatterns: [
       {
