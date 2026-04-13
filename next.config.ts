2ea29931d927a69ce52394a863258b7c9710492a
Dev <macbook@MacBook-Pro--AbuBakr.local>
2026-04-13 06:52:59 +0500

    fix: allow ngrok origin for dev

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  allowedDevOrigins: ["helena-compressed-ungladly.ngrok-free.dev"],
   images: {
     remotePatterns: [
       {
